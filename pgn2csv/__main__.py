eb6860b9cb51a52a60b49f6c3ffb47aedbc63583
Eden <untouchable.cloud@gmail.com>
2024-03-12 16:29:59 +0700

    modify pgn2csv into executable module

---
+++ pgn2csv/__main__.py
@@ -0,0 +1,15 @@
+import argparse
+from .engine import Converter
+
+def main():
+    parser = argparse.ArgumentParser(description="Convert pgn.zst file to csv.")
+    parser.add_argument(
+        "input", type=str, help="name of the pgn.zst file which needs be converted"
+    )
+    args = parser.parse_args()
+    converted_file_name = args.input.split(".")[0] + ".csv"
+    
+    Converter.run(args.input, converted_file_name)
+
+if __name__ == '__main__':
+    main()
--- pgn2csv/engine.py
+++ pgn2csv/engine.py
@@ -3,7 +3,7 @@ import csv
 import subprocess
 
 from .match import Match
-from multiprocessing import JoinableQueue
+from multiprocessing import JoinableQueue, Process
 
 
 TAG_REGEX = re.compile(r'\[(\w+)\s+"([^"]+)"\]')
@@ -125,3 +125,32 @@ class CSVWriter:
                     ]
                 )
                 processing_queue.task_done()
+
+class Converter:
+    @staticmethod
+    def run(input_file_path: str, target_file_path: str):
+        processing_queue = JoinableQueue()
+        parser = PGNParser(input_file_path)
+        csv_writer = CSVWriter(target_file_path)
+
+        process_1 = Process(
+            target=parser.parse_pgn,
+            kwargs=dict(processing_queue=processing_queue),
+        )
+        process_2 = Process(
+            target=csv_writer.write_csv,
+            kwargs=dict(processing_queue=processing_queue),
+        )
+
+        # Start processes
+        process_1.start()
+        process_2.start()
+
+        # Wait for the parser to finish
+        process_1.join()
+
+        # Signal the CSVWrite process to stop by adding None to the queue
+        processing_queue.put(None)
+
+        # Wait for the print process to finish
+        process_2.join()
