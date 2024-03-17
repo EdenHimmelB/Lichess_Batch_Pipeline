6dc9874d31517da244daa535efa01eb10fe16843
Eden <untouchable.cloud@gmail.com>
2024-03-17 16:27:33 +0700

    use dask to reduce memory footprint

--- airflow/dags/chess_monthly_batch_pipeline.py
+++ airflow/dags/chess_monthly_batch_pipeline.py
@@ -6,58 +6,69 @@ from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.python import PythonOperator
 
-import pyarrow.csv as pacsv
-import pyarrow.parquet as papq
-from pyarrow.fs import GcsFileSystem
+import dask.dataframe as dd
+from google.cloud import storage
 
 import requests
-from tqdm import tqdm
+
+client = storage.Client()
+bucket = client.bucket("data_zoomcamp_mage_bucket_1")
 
 
-def download_raw_data_to_local(url: str) -> str:
+def download_data_to_gcs(url: str) -> str:
     # uncompressed_file_name = url.split("/")[-1]
     uncompressed_file_name = "test_run.pgn.zst"
+    blob = bucket.blob(uncompressed_file_name)
 
-    # Send a GET request to the URL
-    with requests.get(url, stream=True) as response:
-        response.raise_for_status()
-        # Retrieve total file size for progress reporting
-        total_size_in_bytes = int(response.headers.get("content-length", 0))
-        block_size = 1024
+    # Stream the download and upload so that temp file isn't needed
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        blob.upload_from_file(r.raw, content_type=r.headers["Content-Type"])
 
-        progress_bar = tqdm(total=total_size_in_bytes, unit="iB", unit_scale=True)
-        with open(uncompressed_file_name, "wb") as file:
-            for data in response.iter_content(block_size):
-                progress_bar.update(len(data))
-                file.write(data)
-        progress_bar.close()
-
-        if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
-            print("ERROR, something went wrong")
-
-    # Push uncompressed file path to the next conversion step
-    return uncompressed_file_name
+    return f"gcs://data_zoomcamp_mage_bucket_1/{uncompressed_file_name}"
 
 
 def convert_raw_data_to_csv(uncompressed_file_path: str) -> str:
-    parsed_file_path = uncompressed_file_path.split(".")[0] + ".csv"
-
-    print(os.listdir())
+    converted_file_path = uncompressed_file_path.split(".")[0] + ".csv"
     subprocess.run(["python3", "-m", "pgn2csv", uncompressed_file_path])
+    return converted_file_path
 
-    return parsed_file_path
 
+def partition_csv_file_to_parquet(csv_file_path: str) -> None:
+    dtype = {
+        "GameID": "str",
+        "Event": "str",
+        "Site": "str",
+        "Date": "str",
+        "Round": "str",
+        "White": "str",
+        "Black": "str",
+        "Result": "str",
+        "UTCDate": "str",
+        "UTCTime": "str",
+        "WhiteElo": "int64",
+        "BlackElo": "int64",
+        "WhiteRatingDiff": "float64",
+        "BlackRatingDiff": "float64",
+        "WhiteTitle": "str",
+        "BlackTitle": "str",
+        "ECO": "str",
+        "Opening": "str",
+        "TimeControl": "str",
+        "Termination": "str",
+        "GameMoves": "str",
+    }
 
-def partition_csv_file_to_parquet(
-    csv_file_path: str, parquet_output_path: str, partition_column: str
-) -> None:
-    gcs = GcsFileSystem()
-    table = pacsv.read_csv(csv_file_path)
-    papq.write_to_dataset(
-        table=table,
-        root_path=parquet_output_path,
-        partition_cols=[partition_column],
-        filesystem=gcs,
+    ddf: dd.DataFrame = dd.read_csv(
+        csv_file_path,
+        storage_options={"token": "google_default"},
+        dtype=dtype,
+        assume_missing=True,
+    )
+    ddf.to_parquet(
+        "gcs://data_zoomcamp_mage_bucket_1/dask_output",
+        storage_options={"token": "google_default"},
+        write_index=False,
     )
 
 
@@ -77,14 +88,14 @@ with DAG(
 
     download_task = PythonOperator(
         task_id="download_file",
-        python_callable=download_raw_data_to_local,
+        python_callable=download_data_to_gcs,
         op_kwargs={
             "url": "https://drive.google.com/uc?export=download&id=1d7YF54Fij2yfXECZhQpF8jAB-98S99hi&filename=small.pgn.zst"
         },
         provide_context=True,
     )
 
-    parse_task = PythonOperator(
+    conversion_task = PythonOperator(
         task_id="convert_pgn_zst_to_csv_format",
         python_callable=convert_raw_data_to_csv,
         op_kwargs={
@@ -93,11 +104,12 @@ with DAG(
         provide_context=True,
     )
 
-    # upload_to_gcs_as_parquet_task = PythonOperator(
-    #     task_id="upload_to_gcs",
-    #     python_callable=partition_csv_file_to_parquet,
-    #     op_kwargs={},
-    # )
+    upload_to_gcs_as_parquet_task = PythonOperator(
+        task_id="upload_to_gcs",
+        python_callable=partition_csv_file_to_parquet,
+        op_kwargs={
+            "csv_file_path": "{{ ti.xcom_pull(task_ids='convert_pgn_zst_to_csv_format') }}"
+        },
+    )
 
-    download_task >> parse_task
-    # >> upload_to_gcs_as_parquet_task
+    download_task >> conversion_task >> upload_to_gcs_as_parquet_task
