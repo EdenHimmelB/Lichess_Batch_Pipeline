5f002fe7953dd1c86a51bd2c7f9d832696b02229
Eden <untouchable.cloud@gmail.com>
2024-03-31 00:02:16 +0700

    create test case for sparksubmitoperator

--- airflow/spark-jobs/convert_csv_to_parquet.py
+++ airflow/spark-jobs/convert_csv_to_parquet.py
@@ -35,32 +35,18 @@ schema = StructType(
 )
 
 
-def convert_csv_to_parquet(csv_file_path, output_path):
-    spark = (
-        SparkSession.builder.appName("CSV to Parquet Conversion")
-        .config("spark.jars", "gcs-connector-hadoop3-2.2.21-shaded.jar")
-        .config(
-            "spark.hadoop.fs.gs.impl",
-            "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem",
-        )
-        .config(
-            "spark.hadoop.fs.AbstractFileSystem.gs.impl",
-            "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS",
-        )
-        .config("fs.gs.auth.service.account.enable", "true")
-        # .config("spark.hadoop.fs.gs.auth.service.account.json.keyfile", "/opt/spark/google_credentials.json")
-        .config(
-            "fs.gs.auth.service.account.json.keyfile",
-            "./opt/spark/google_credentials.json",
-        )
+def convert_csv_to_parquet():
+    spark: SparkSession = (
+        SparkSession.builder.master("local[*]")
+        .appName("CSV to Parquet Conversion")
         .getOrCreate()
     )
-    df = spark.read.csv(csv_file_path, header=True, schema=schema)
-    df.write.parquet(output_path)
+    df = spark.read.csv(
+        "gs://data_zoomcamp_mage_bucket_1/mock_data.csv", header=True, schema=schema
+    )
+    df.write.parquet("gs://data_zoomcamp_mage_bucket_1/mock_parquet_folder")
     spark.stop()
 
 
 if __name__ == "__main__":
-    input_path = sys.argv[1]
-    output_path = sys.argv[2]
-    convert_csv_to_parquet(input_path, output_path)
+    convert_csv_to_parquet()
